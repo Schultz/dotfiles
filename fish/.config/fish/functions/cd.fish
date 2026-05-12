c9aac3bc37e1b28fdd780c2faa7f5cbe7e3e02d2
Rob Schultz <1008+Schultz@users.noreply.github.com>
2026-05-11 19:08:51 -0700

    fix zed ssh'ing into server

--- fish/.config/fish/functions/cd.fish
+++ fish/.config/fish/functions/cd.fish
@@ -1,11 +1,15 @@
 function cd --description 'list files in the current directory after changing to it'
-    if count $argv >/dev/null
-        # prevents recurse infinitely by using built-in cd
-        builtin cd "$argv"; and eza --long --all --header --git --icons --no-permissions --no-time --no-user --no-filesize --group-directories-first
-        # builtin cd "$argv"; and yazi
+    if status is-interactive
+        if count $argv >/dev/null
+            # prevents recurse infinitely by using built-in cd
+            builtin cd "$argv"; and eza --long --all --header --git --icons --no-permissions --no-time --no-user --no-filesize --group-directories-first
+            # builtin cd "$argv"; and yazi
+        else
+            # builtin cd ~; and eza -lagh --icons --group-directories-first
+            builtin cd ~; and eza --long --all --header --git --icons --no-permissions --no-time --no-user --no-filesize --group-directories-first
+            # builtin cd "$argv"; and yazi
+        end
     else
-        # builtin cd ~; and eza -lagh --icons --group-directories-first
-        builtin cd ~; and eza --long --all --header --git --icons --no-permissions --no-time --no-user --no-filesize --group-directories-first
-        # builtin cd "$argv"; and yazi
+        builtin cd $argv
     end
 end
